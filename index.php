95a34cbc0436b2f1c563e865bf60c92c20244566
Gavin <gavinmcd94@gmail.com>
2022-06-15 19:56:28 +0100

    add theme settings
    - can set social media URLs

--- index.php
+++ index.php
@@ -1,11 +1,24 @@
-<?php get_header(); ?>
+<?php get_header(); 
+
+$twitterURL = get_option('twitter_url'); 
+$emailURL = get_option('email_url');
+$linkedinURL = get_option('linkedin_url');
+?>
 <main>
     <h1>Shannon Hughes Spence</h1>
     <img src="<?php echo get_bloginfo('template_directory'); ?>/shannon.jpg" class="user-image"/>
     <ul class="social">
-        <li><a href="mailto:shannonhughesspence@gmail.com" target="_blank" rel="noopener noreferrer"><span class="fa-solid fa-3x fa-square-envelope"></span></a></li>
-        <li><a href="#" target="_blank" rel="noopener noreferrer"><span class="fa-brands fa-3x fa-linkedin"></span></a></li>
-        <li><a href="https://twitter.com/sionainnohaodh" target="_blank" rel="noopener noreferrer"><span class="fa-brands fa-3x fa-twitter-square"></span></a></li>
+
+        
+        <?php if(str_contains($emailURL, "@")) : ?>
+            <li><a href="mailto:<?php echo $emailURL?>" target="_blank" rel="noopener noreferrer"><span class="fa-solid fa-3x fa-square-envelope"></span></a></li>
+        <?php endif; ?>
+        <?php if(str_contains($linkedinURL, "linked")) : ?>
+            <li><a href="<?php echo $linkedinURL?>" target="_blank" rel="noopener noreferrer"><span class="fa-brands fa-3x fa-linkedin"></span></a></li>
+        <?php endif; ?>
+        <?php if(str_contains($twitterURL, "twitter")) : ?>
+            <li><a href="<?php echo $twitterURL?>" target="_blank" rel="noopener noreferrer"><span class="fa-brands fa-3x fa-twitter-square"></span></a></li>
+        <?php endif; ?>
         </ul>
         <p class="user-bio">Research Assistant 📚 MSc Sociology 👩‍🎓 Researching gender, health, the occult, and the body 📖 Chess amateur ♟ Knitting enthusiast 🧶 (I promise I'm cool)</p>
 
