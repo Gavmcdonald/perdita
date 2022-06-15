64cc67e9bbb92dfc2045348208a858f2bd8687e8
Gavin <gavinmcd94@gmail.com>
2022-06-15 01:01:52 +0100

    Added font awesome and updated links
    - added social icons
    - added profile image
    - changed bio to actual bio

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <?php get_header(); ?>
 <main>
     <h1>Shannon Hughes Spence</h1>
-    <div class="user-image"></div>
+    <img src="<?php echo get_bloginfo('template_directory'); ?>/shannon.jpg" class="user-image"/>
     <ul class="social">
-        <li></li>
-        <li></li>
-        <li></li>
-    </ul>
-    <p class="user-bio">This is where all the lovely things about Shannon will go. There will be lots of lovely
-        things here... lots and lots. All sorts of things about how smart she knowing things and all</p>
+        <li><a href="mailto:shannonhughesspence@gmail.com" target="_blank" rel="noopener noreferrer"><span class="fa-solid fa-3x fa-square-envelope"></span></a></li>
+        <li><a href="#" target="_blank" rel="noopener noreferrer"><span class="fa-brands fa-3x fa-linkedin"></span></a></li>
+        <li><a href="https://twitter.com/sionainnohaodh" target="_blank" rel="noopener noreferrer"><span class="fa-brands fa-3x fa-twitter-square"></span></a></li>
+        </ul>
+        <p class="user-bio">Research Assistant 📚 MSc Sociology 👩‍🎓 Researching gender, health, the occult, and the body 📖 Chess amateur ♟ Knitting enthusiast 🧶 (I promise I'm cool)</p>
 
 </main>
 <?php get_footer(); ?>
--- singular.php
+++ singular.php
@@ -1,18 +1,18 @@
 <?php get_header(); ?>
 <main class="post">
-        <h2 class="blog-post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <h2 class="blog-post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
-        <?php the_content(); ?>
-        <hr>
-        <div class="post-foot">
-            <div class="post-info">
-                <p class="post-date"><?php echo get_the_date(); ?></p>
-                <ul class="social">
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
-            </div>
-      
+    <?php the_content(); ?>
+    <hr>
+    <div class="post-foot">
+        <div class="post-info">
+            <p class="post-date"><?php echo get_the_date(); ?></p>
+            <ul class="social">
+                <li><a href="mailto:shannonhughesspence@gmail.com" target="_blank" rel="noopener noreferrer"><span class="fa-solid fa-2x fa-square-envelope"></span></a></li>
+                <li><a href="#" target="_blank" rel="noopener noreferrer"><span class="fa-brands fa-2x fa-linkedin"></span></a></li>
+                <li><a href="https://twitter.com/intent/tweet" target="_blank" rel="noopener noreferrer"><span class="fa-brands fa-2x fa-twitter-square"></span></a></li>
+            </ul>
+        </div>
+
 </main>
 <?php get_footer(); ?>
