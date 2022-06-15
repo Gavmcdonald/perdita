aa763747cbba476977e12b67f70976f6c3ad82bd
Gavin <gavinmcd94@gmail.com>
2022-06-15 22:39:26 +0100

    finished homepage options

--- functions.php
+++ functions.php
@@ -1,19 +1,20 @@
 <?php
 
-function theme_settings_page(){
+function theme_settings_page()
+{
 
-    ?>
-	    <div class="wrap">
-	    <h1>Theme Settings</h1>
-	    <form method="post" action="options.php">
-	        <?php
-	            settings_fields("section");
-	            do_settings_sections("theme-options");      
-	            submit_button(); 
-	        ?>          
-	    </form>
-		</div>
-	<?php
+?>
+	<div class="wrap">
+		<h1>Theme Settings</h1>
+		<form method="post" action="options.php">
+			<?php
+			settings_fields("section");
+			do_settings_sections("theme-options");
+			submit_button();
+			?>
+		</form>
+	</div>
+<?php
 
 }
 
@@ -24,50 +25,63 @@ function add_theme_menu_item()
 
 add_action("admin_menu", "add_theme_menu_item");
 
+function display_name_element()
+{
+?>
+	<input type="text" name="name" id="name" value="<?php echo get_option('name'); ?>" />
+<?php
+}
 function display_twitter_element()
 {
-	?>
-    	<input type="text" name="twitter_url" id="twitter_url" value="<?php echo get_option('twitter_url'); ?>" />
-    <?php
+?>
+	<input type="text" name="twitter_url" id="twitter_url" value="<?php echo get_option('twitter_url'); ?>" />
+<?php
 }
 
 function display_linkedin_element()
 {
-	?>
-    	<input type="text" name="linkedin_url" id="linkedin_url" value="<?php echo get_option('linkedin_url'); ?>" />
-    <?php
+?>
+	<input type="text" name="linkedin_url" id="linkedin_url" value="<?php echo get_option('linkedin_url'); ?>" />
+<?php
 }
 function display_email_element()
 {
-	?>
-    	<input type="text" name="email_url" id="email_url" value="<?php echo get_option('email_url'); ?>" />
-    <?php
+?>
+	<input type="text" name="email_url" id="email_url" value="<?php echo get_option('email_url'); ?>" />
+<?php
 }
 function display_image_element()
 {
-	?>
-    	<input type="text" name="image_url" id="image_url" value="<?php echo get_option('image_url'); ?>" />
-    <?php
+?>
+	<input type="text" name="image_url" id="image_url" value="<?php echo get_option('image_url'); ?>" />
+<?php
+}
+function display_bio_element()
+{
+?>
+	<textarea cols="40" rows="5" type="text" name="bio" id="bio"><?php echo get_option('bio'); ?></textarea>
+<?php
 }
 
 function display_theme_panel_fields()
 {
-	add_settings_section("section", "All Settings", null, "theme-options");
-	
-	add_settings_field("twitter_url", "Twitter Profile Url", "display_twitter_element", "theme-options", "section");
-    add_settings_field("linkedin_url", "LinkedIn Profile Url", "display_linkedin_element", "theme-options", "section");
-    add_settings_field("email_url", "Send To Email Address", "display_email_element", "theme-options", "section");
-    add_settings_field("image_url", "User Image URL", "display_image_element", "theme-options", "section");
-	
-    register_setting("section", "twitter_url");
-    register_setting("section", "linkedin_url");
+	add_settings_section("section", "Home Page Settings", null, "theme-options");
+
+	add_settings_field("name", "Profile Name", "display_name_element", "theme-options", "section");
+	add_settings_field("image_url", "User Image URL", "display_image_element", "theme-options", "section");
+	add_settings_field("twitter_url", "Twitter Profile URL", "display_twitter_element", "theme-options", "section");
+	add_settings_field("linkedin_url", "LinkedIn Profile URL", "display_linkedin_element", "theme-options", "section");
+	add_settings_field("email_url", "Send To Email Address", "display_email_element", "theme-options", "section");
+	add_settings_field("bio", "User bio", "display_bio_element", "theme-options", "section");
+
+	register_setting("section", "name");
+	register_setting("section", "twitter_url");
+	register_setting("section", "linkedin_url");
 	register_setting("section", "email_url");
 	register_setting("section", "image_url");
+	register_setting("section", "bio");
 }
 
 add_action("admin_init", "display_theme_panel_fields");
 
 ?>
-
-
-
--- index.php
+++ index.php
@@ -1,29 +1,37 @@
-<?php get_header(); 
+<?php get_header();
 
-$twitterURL = get_option('twitter_url'); 
+$twitterURL = get_option('twitter_url');
 $emailURL = get_option('email_url');
 $linkedinURL = get_option('linkedin_url');
 $imageURL = get_option('image_url');
+$bio = get_option('bio');
+$name = get_option('name')
 ?>
 <main>
-    <h1>Shannon Hughes Spence</h1>
-    <?php if(!empty($emailURL)) : ?>
-        <img src="<?php echo $imageURL; ?>" class="user-image"/>
+
+    <?php if (!empty($name)) : ?>
+        <h1><?php echo $name; ?></h1>
+    <?php endif; ?>
+    <?php if (!empty($imageURL)) : ?>
+        <img src="<?php echo $imageURL; ?>" class="user-image" />
     <?php endif; ?>
     <ul class="social">
 
-        
-        <?php if(str_contains($emailURL, "@")) : ?>
-            <li><a href="mailto:<?php echo $emailURL?>" target="_blank" rel="noopener noreferrer"><span class="fa-solid fa-3x fa-square-envelope"></span></a></li>
+
+        <?php if (str_contains($emailURL, "@")) : ?>
+            <li><a href="mailto:<?php echo $emailURL ?>" target="_blank" rel="noopener noreferrer"><span class="fa-solid fa-3x fa-square-envelope"></span></a></li>
         <?php endif; ?>
-        <?php if(str_contains($linkedinURL, "linked")) : ?>
-            <li><a href="<?php echo $linkedinURL?>" target="_blank" rel="noopener noreferrer"><span class="fa-brands fa-3x fa-linkedin"></span></a></li>
+        <?php if (str_contains($linkedinURL, "linked")) : ?>
+            <li><a href="<?php echo $linkedinURL ?>" target="_blank" rel="noopener noreferrer"><span class="fa-brands fa-3x fa-linkedin"></span></a></li>
         <?php endif; ?>
-        <?php if(str_contains($twitterURL, "twitter")) : ?>
-            <li><a href="<?php echo $twitterURL?>" target="_blank" rel="noopener noreferrer"><span class="fa-brands fa-3x fa-twitter-square"></span></a></li>
+        <?php if (str_contains($twitterURL, "twitter")) : ?>
+            <li><a href="<?php echo $twitterURL ?>" target="_blank" rel="noopener noreferrer"><span class="fa-brands fa-3x fa-twitter-square"></span></a></li>
         <?php endif; ?>
-        </ul>
-        <p class="user-bio">Research Assistant 📚 MSc Sociology 👩‍🎓 Researching gender, health, the occult, and the body 📖 Chess amateur ♟ Knitting enthusiast 🧶 (I promise I'm cool)</p>
+    </ul>
+
+    <?php if (!empty($bio)) : ?>
+        <p class="user-bio"><?php echo $bio; ?></p>
+    <?php endif; ?>
 
 </main>
 <?php get_footer(); ?>
