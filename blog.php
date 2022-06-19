c377628817c08d5bcf5334ca8053baa3e3214569
Gavin <gavinmcd94@gmail.com>
2022-06-19 12:17:25 +0100

    Add pagination to blog page

--- blog.php
+++ blog.php
@@ -4,22 +4,32 @@
 ?>
 <?php get_header(); ?>
 <main>
-<?php
- 
- // The Query
- $the_query = new WP_Query( array('post_type' => 'post') );
-  
- // The Loop
- if ( $the_query->have_posts() ) {
-     while ( $the_query->have_posts() ) {
-         $the_query->the_post();
-         get_template_part('content', get_post_format());
-     }
- } else {
-     // no posts found
- }
- /* Restore original Post Data */
- wp_reset_postdata();
-    ?>
+    <?php
+
+    // sets the 'paged' parameter for pagination
+    $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+
+    // The Query
+    $the_query = new WP_Query(array('post_type' => 'post', 'paged' => $paged));
+
+    // The Loop
+    if ($the_query->have_posts()) {
+        while ($the_query->have_posts()) {
+            $the_query->the_post();
+            get_template_part('content', get_post_format());
+        }
+
+    ?><nav>
+            <ul class="pager">
+                <li><?php next_posts_link('Previous', $the_query->max_num_pages); ?></li>
+                <li><?php previous_posts_link('Next', $the_query->max_num_pages); ?></li>
+            </ul>
+        </nav> <?php
+            } else {
+                // no posts found
+            }
+            /* Restore original Post Data */
+            wp_reset_postdata();
+                ?>
 </main>
 <?php get_footer(); ?>
